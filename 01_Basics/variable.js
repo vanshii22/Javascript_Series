fb1ab77d4f0282e2a06d983b8ef2660803c4749c
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-10 04:08:57 +0000

    variables

---
+++ 01_Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId=4365;
+let accountName="Vanshika Sharma";
+var accountEmail="vanshikasharma9456@gmail.com";
+accountCity="Jaipur";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountName,accountCity]);
+
+// accountId=768;
+accountName="Vanshi Sharma";
+let accountHolder;
+console.log(accountHolder);
