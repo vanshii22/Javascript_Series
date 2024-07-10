107c56189174acf5bc92af99d9ca359edb0ce71b
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-10 04:59:43 +0000

    conversion

---
+++ 01_Basics/conversionoperation.js
@@ -0,0 +1,18 @@
+// let score="33";
+// console.log(typeof score);
+
+// let scoreInNumber=Number(score)=>Conversion in number
+// console.log(scoreInNumber);
+
+// let nameStudent=33;
+// console.log(typeof(nameStudent));
+//  let isBoolean=Boolean(nameStudent);
+//  console.log(typeof(isBoolean));   
+//  console.log(isBoolean);         
+
+
+let nameStudent=33;
+console.log(typeof(nameStudent));
+ let isString=String(nameStudent);
+ console.log(typeof(isString));   
+ console.log(isString);  
