97356610b44de692224bbfba7dccdbbefc239834
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-10 04:39:41 +0000

    data types

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"//we use it to treat all code as newer version of js code 
+
+// alert("Hello");//we are using node js just because of it is giving error
+
+let name="vanshi";
+let age=21;
+let isLoggedIn=true;
+console.log(typeof("vanshi"));
+
+
+// Primitive data types are 
+// number -2 to the power of 53
+// bigint
+//string 
+//boolean
+// null - standalone value but its data type is object 
+// undefined - its data type is undefined
+// symbol-unique
+let sirname;
+let temp=null;
+console.log(typeof(null));
+console.log(typeof(sirname));
