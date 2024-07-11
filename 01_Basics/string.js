c85d5043d4e9f018b7c4439036bf6b1d570ae3ba
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-11 03:40:42 +0000

    string

---
+++ 01_Basics/string.js
@@ -0,0 +1,38 @@
+const name="Vanshu";
+const repocount="50";
+console.log(`My name is ${name} and my repocount is ${repocount}`);
+
+
+// New method to define a String is 
+
+const userOne=new String("Vanshika");
+console.log(userOne[0]);
+
+
+// String methods
+// console.log(userOne.__proto__);
+// console.log(userOne.length);
+// console.log(userOne.toUpperCase());
+// console.log(userOne.charAt("0"));
+// console.log(userOne.indexOf("k"));
+// console.log(userOne.substring(0,4));
+// console.log(userOne.slice(1,3));
+// console.log(userOne.slice(-8,4));
+
+
+// const userTwo=new String("  Sachin  ");
+// console.log(userTwo);
+// console.log(userTwo.trim());
+// console.log(userTwo.trimStart());
+
+
+// const url="vanshi@%20gmail.com";
+// console.log(url.replace('@%20','-'));
+
+// console.log(url.includes("sundar"));
+
+const newString="Vanshi-Sharma";
+console.log(newString.split('-'));
+
+
+
