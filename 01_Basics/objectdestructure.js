76dd5352ca336df6b070557aefebfb98c1a46d79
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-12 10:53:19 +0000

    object destructure

---
+++ 01_Basics/objectdestructure.js
@@ -0,0 +1,17 @@
+//Object destructure
+const course={
+    courseId:1,
+    couseName:"HTML"
+}
+const{courseId:Id}=course
+// console.log(courseId);
+console.log(Id);
+
+
+
+
+// Api in Json format-In json both side key and value pair should be in double quotes
+// {
+//     "name":"Vanshi",
+//     "email":"google.com"
+// }
