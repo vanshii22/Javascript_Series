efb4c1c76d2df3800b772f971f20ab4e04ffb7ea
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-16 05:47:00 +0000

    whileanddowhile.js

---
+++ 01_Basics/whileanddowhile.js
@@ -0,0 +1,25 @@
+// let index=0;
+// while (index<10) {
+//     console.log(`The value of index is ${index}`);
+//     index=index+2
+// }
+
+// let myArray=['flash','superman','spiderman']
+// let index=0;
+// while (index < myArray.length) {
+//   const element=myArray[index]
+//   console.log(element);
+//   index++;
+    
+// }
+
+// let index=0;
+// do {
+//     console.log(`The index value are :${index}`);
+//     index++;
+// } while (index<10);
+let index=11;
+do {
+    console.log(`The index value are :${index}`);
+    index++;
+} while (index<10);
