8471a38390fe62146f60aa8bbc38acae3f6909e8
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-16 05:29:40 +0000

    forloop

---
+++ 01_Basics/forloop.js
@@ -0,0 +1,46 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j}=${i*j}`);
+        
+//     }
+    
+// }
+
+// const array=["Apple","Mango","Banana"]
+// console.log(array.length);
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+    
+// }
+
+// Break and Continue
+
+// for (let i = 0; i < 10; i++) {
+//    if(i==5){
+//     console.log("5 is detected");
+//     break;
+//    }
+//    console.log(`The value of i is ${i}`);
+// }
+// for (let i = 0; i < 10; i++) {
+//     if(i==5){
+//      console.log("5 is detected");
+//      continue;
+//     }
+//     console.log(`The value of i is ${i}`);
+//  }
+for (let i = 0; i < 10; i++) {
+    if(i==5){
+     console.log("5 is detected");
+     
+    }
+    console.log(`The value of i is ${i}`);
+ }
