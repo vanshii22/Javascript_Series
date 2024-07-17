c3cf0e56a458c986e338db0637221247afa6a804
Vanshika Sharma <106140210+vanshii22@users.noreply.github.com>
2024-07-17 03:53:55 +0000

    test.js

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+for (let i = 1; i <= 100; i++) {
+    console.log(`The table of i is: ${i}`);
+    for (let j = 1; j <= 100; j++) {
+        console.log(`${i} * ${j}=${i*j}`);
+        
+    }
+    
+}
